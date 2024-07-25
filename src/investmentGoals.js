c48ca8c0fa1f8867834cf83edbd4f49803ff33f2
victormanuelLR <vleal3423@gmail.com>
2024-07-25 15:54:27 -0300

    Formulário de investimento criado e arquivo de cálculo de projeções criado

--- src/investmentGoals.js
+++ src/investmentGoals.js
@@ -9,4 +9,54 @@ function genarateReturnsArray(
   monthlyContribution = 0,
   returRate = 0,
   returnTimeFrame = "monthly"
-) {}
+) {
+  if (!startingAmount && !timeHorizon) {
+    throw new Error(
+      "Investimento inicial e prazo devem ser prenchidos com valores positivos"
+    );
+  }
+
+  const finalReturnRate =
+    returnTimeFrame === "monthly"
+      ? 1 + returRate / 100
+      : convertToMonthlyReturnRate(1 + returRate / 100);
+
+  const finalyTimeHorizon =
+    timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestmentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnsArray = [referenceInvestmentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalyTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+
+    const interestReturns =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+
+    const totalInterestReturns = totalAmount - investedAmount;
+
+    returnsArray.push({
+      investedAmount,
+      interestReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+    });
+  }
+
+  return returnsArray;
+}
