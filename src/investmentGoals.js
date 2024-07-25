c5dc1e00f1bd91d7946dba8c667fc0e02117537f
victormanuelLR <vleal3423@gmail.com>
2024-07-24 21:09:57 -0300

    começando a preparar o código para calcular os investimentos

---
+++ src/investmentGoals.js
@@ -0,0 +1,12 @@
+function convertToMonthlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+}
+
+function genarateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = "monthly",
+  monthlyContribution = 0,
+  returRate = 0,
+  returnTimeFrame = "monthly"
+) {}
